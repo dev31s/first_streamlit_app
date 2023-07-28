931be0b9a35732292bcf0e851b9f25672b7e4889
dev31s <140840106+dev31s@users.noreply.github.com>
2023-07-28 21:09:39 +0200

    Update streamlit_app.py
    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 
 streamlit.title("My Mom's New Healthy Diner")
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
